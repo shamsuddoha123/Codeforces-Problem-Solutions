cce18e1beb9edb28f21a767b37511f3c824aa094
Md. Shamsuddoha <154912721+shamsuddoha123@users.noreply.github.com>
2024-04-08 03:40:33 +0600

    Update 1367A - Short Substrings.c

--- 1367A - Short Substrings.c
+++ 1367A - Short Substrings.c
@@ -3,24 +3,22 @@
 int main()
 {
     char str[1000];
-    int duplicate[1000];
     int i,t,len;
     scanf("%d", &t);
     while(t--)
     {
-        gets(str);
+        scanf("%s", str);
         len=strlen(str);
         printf("%c", str[0]);
-        for(i=1;i<(len-1);i++)
+        for(i=1;i<(len-1);i+=2)
         {
-            if(duplicate[str[i]]==0)
+            if(str[i]==str[i+1])
             {
                 printf("%c", str[i]);
             }
-            duplicate[str[i]]=1;
         }
-        printf("%c", str[len]);
-
+        printf("%c", str[len-1]);
+        printf("\n");
     }
     return 0;
 }
