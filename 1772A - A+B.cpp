3ee0ca32b79f3228e6b8f22b2d32a5c6909e7a6a
Md. Shamsuddoha <154912721+shamsuddoha123@users.noreply.github.com>
2024-04-16 09:27:59 +0600

    Add files via upload

---
+++ 1772A - A+B.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int t,a,b,sum;
+    char ch;
+    cin>>t;
+    while(t--)
+    {
+        cin>>a>>ch>>b;
+        sum=0;
+        sum=a+b;
+        cout<<sum<<endl;
+    }
+    return 0;
+}
