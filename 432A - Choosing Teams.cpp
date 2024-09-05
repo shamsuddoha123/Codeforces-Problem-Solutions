ec07d5e856949503499af0c30ce0f1fdd9477abc
Md. Shamsuddoha <154912721+shamsuddoha123@users.noreply.github.com>
2024-09-05 11:15:43 +0600

    Add files via upload

---
+++ 432A - Choosing Teams.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n, k, i, count = 0;
+    cin >> n >> k;
+    int arr[n];
+    for (i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    for (i = 0; i < n; i++)
+    {
+        if (arr[i] < 5)
+        {
+            if (5 - arr[i] >= k)
+            {
+                {
+                    count++;
+                }
+            }
+        }
+    }
+    if (count >= 3)
+    {
+        cout << count / 3 << endl;
+    }
+    else
+    {
+        cout << 0 << endl;
+    }
+    return 0;
+}
